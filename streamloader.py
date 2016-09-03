3be026a82a77ac49ba389ea41738dc1a02df1ad3
martinxtm <martinocker@gmx.de>
2016-09-03 17:46:38 +0200

    init and isnear function

--- streamloader.py
+++ streamloader.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 import socket
 import json
+import numpy as np
 
 class StreamLoader():
+    def __init__(self):
+	self.stream = self.printStream()
+	pass
 
-    def getStream(self):
+    def printStream(self):
         print "running printStream()"
         #create an INET, STREAMing socket
         sock = socket.socket(
@@ -20,13 +24,39 @@ class StreamLoader():
             if not line: break
             yield json.loads(line)
 
-    def processEvents(self,stream):
-        print stream
-        for event in stream:
-            print "-----"
-            print event
-            print ""
+    def processEvents(self):
+	print self.stream
+	for event in self.stream:
+	    #print(event)
+	    if event['msgid'] == 5:
+                print(event['name'], event['userid'], event['imo'])
+		image_url = ['http://www.vesseltracker.com/en/Ships/'+ str(event['imo']) +'.html']
+		print image_url
+	#	print(result['userid'], result['pos'])
+	#	print result.keys() 
+
+    def saveEvents(self):
+	with open('streamdata.json', 'a+') as f:
+    	     json.dump(event, f)
+
+    def isnear(self):
+	print self.stream
+	radius = 0.0128
+	for event in self.stream:
+	    if event['msgid'] == 1:
+		distance = np.sqrt((9.95 - event['pos'][0])**2 + (53.544 - event['pos'][1])**2)
+		if distance <= radius:
+			is_near = True
+			print('this ship is near: ', event['userid']) 
+		else:
+			is_near = False
+
+
 
 app = StreamLoader()
-app.processEvents(app.getStream())
+#app.processEvents(app.printStream())
+app.processEvents()
+#app.isnear()
+
+
 
